33970e679030994fdc177f46b1983b08560bebe4
Almaash <alamalmaash@gmail.com>
2025-08-02 00:27:53 +0530

    bugs fixed added checks 2

--- src/app/api/BackendUrl.ts
+++ src/app/api/BackendUrl.ts
@@ -2,6 +2,6 @@
 // const BackendUrl = 'http://localhost:5000' 
 
 // production
-const BackendUrl = 'http://194.164.151.98:5000' 
+const BackendUrl = 'http://194.164.151.98:5000'
 
 export default BackendUrl;
